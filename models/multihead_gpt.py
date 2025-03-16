74afbbef5a177db8978d73f6aef3b0dd174decea
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2025-03-16 23:38:24 +0000

    basic target computation added

--- models/multihead_gpt.py
+++ models/multihead_gpt.py
@@ -20,14 +20,34 @@ class CrossEntropyLoss(torch.nn.Module):
         if self.apply_log_softmax:
             pred_log_probs = torch.nn.functional.log_softmax(pred_log_probs, dim=-1)
         keep_mask = target[:, :, 0:1] != self.ignore_idx  # assumes that all of the indicates are ignored if true (output shape: [B, L, 1])
-        num_elements_kept = int(keep_mask.sum())
+        num_elements_kept = max(int(keep_mask.sum()), 1)  # should normalize by at least by 1
         loss = - (keep_mask * target * pred_log_probs).sum(dim=-1).sum() / num_elements_kept  # B x L x V
         return loss
 
 
-def compute_targets(targets: torch.Tensor, head_size: int) -> torch.Tensor:
-    assert len(targets.shape) == 2, targets.shape  # B x L
-    raise NotImplementedError
+def compute_targets(input_ids: torch.Tensor, vocab_size: int, head_size: int, boundary_condition: str = "normalize") -> torch.Tensor:
+    """
+    Boundary condition defines how we deal with the boundary condition as we reach the end of the sequence.
+    - normalize: just renormalize the sequence based on the available elements and count
+    - ignore: normalize and ignore the mass assigned the sink token (assumes that there is a sink token) -- results in an unnormalized distribution
+    - sink: have a separate sink token, similar to the BOS token
+    """
+    assert boundary_condition in ["normalize", "ignore", "sink"], boundary_condition
+    assert head_size >= 1, head_size
+    assert len(input_ids.shape) == 2, input_ids.shape  # B x L
+    B, seq_len = input_ids.shape
+
+    relative_freq = torch.zeros(B, seq_len-1, vocab_size).to(input_ids.device)  # B x (L-1) x V
+    weights = torch.ones_like(relative_freq[:, 0, :])  # B x V
+    for start_idx in range(1, seq_len):  # targets are one token less than the sequence
+        last_idx = min(start_idx + head_size, seq_len)
+        current_input_chunk = input_ids[:, start_idx:last_idx]
+        relative_freq[:, start_idx-1, :] = torch.scatter_add(relative_freq[:, start_idx-1, :], dim=1, index=current_input_chunk, src=weights)
+    if boundary_condition == "normalize":
+        relative_freq = relative_freq / relative_freq.sum(dim=-1, keepdim=True)  # normalize by the actual frequency
+    else:
+        raise NotImplementedError(f"Boundary condition {boundary_condition} not implemented!")
+    return relative_freq
 
 
 class MultiheadGPT(Transformer):
@@ -74,8 +94,9 @@ class MultiheadGPT(Transformer):
 
             if targets is not None:
                 head_logits = self.lm_head(head_output)
+                vocab_size = head_logits.shape[-1]  # B x L x V
                 # Calculate loss with ignore_index=-1, meaning we skip the gradient contributions from those tokens which is basically the prefix tokens
-                head_targets = compute_targets(targets, head_size)
+                head_targets = compute_targets(targets, vocab_size, head_size)
                 head_loss = self.loss_fn(head_logits, head_targets)
                 total_loss += head_loss * self.head_weights[head_idx]
                 if head_idx == 0:
