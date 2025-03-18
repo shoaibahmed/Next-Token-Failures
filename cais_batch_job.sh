baaf4ef2df9cb689233ff012ff1b7e3df1b2bd23
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2025-03-18 05:13:06 +0000

    boundary condition arg added

--- cais_batch_job.sh
+++ cais_batch_job.sh
@@ -15,7 +15,7 @@ echo "Batch script head node IP: ${MASTER_ADDR} / # nodes: ${SLURM_JOB_NUM_NODES
 n_proc=1
 args="--model gpt --n_layer 6 --n_embd 384 --n_head 6 --n_train 200000 --n_test 20000  --batch_size 256"
 args="${args} --dataset graph --deg 2 --path 5 --num_nodes 50 --lr 0.0001 --eval_every 1 --use_wandb"
-# args="${args} --prediction_head_sizes 1,2,4,8 --prediction_head_weights 1,1,1,1"
+# args="${args} --prediction_head_sizes 1,2,4,8 --prediction_head_weights 1,1,1,1 --multihead_boundary_condition normalize"
 # args="${args} --teacherless"
 # args="${args} --reverse"
 
