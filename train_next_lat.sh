5608d30b305ca3540ca388aa17ca5198ff150986
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2025-11-23 17:28:12 +0000

    eval setting updated

--- train_next_lat.sh
+++ train_next_lat.sh
@@ -4,7 +4,7 @@ n_proc=1
 
 args="--model next_lat_gpt"
 args="${args} --n_layer 6 --n_embd 384 --n_head 6 --n_train 200000 --n_test 20000  --batch_size 256"
-args="${args} --dataset graph --deg 2 --path 5 --num_nodes 50 --lr 0.0001 --waypoint_len 4"
+args="${args} --dataset graph --deg 2 --path 10 --num_nodes 100 --lr 0.0001 --waypoint_len 9"
 args="${args} --use_wandb --save_checkpoints --eval_every 1"
 
 bash -c "torchrun --nproc-per-node ${n_proc} --standalone train.py ${args}"
